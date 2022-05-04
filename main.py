63481386c2bac9232a3d154cd06cb3fad9ecdbe6
melvinchia3636 <melvinchia623600@gmail.com>
2022-05-04 22:00:19 +0800

    added more sections

--- main.py
+++ main.py
@@ -2,12 +2,86 @@ import sys
 import pygame
 import cairosvg
 import io
+import numpy as np
 
 def load_svg(filename):
     new_bites = cairosvg.svg2png(url = filename)
     byte_io = io.BytesIO(new_bites)
     return pygame.image.load(byte_io)
 
+# Adapted from https://github.com/lordmauve/pgzero/blob/master/pgzero/ptext.py#L81-L143
+def wrap_text(text, font, max_width):
+    texts = text.replace("\t", "    ").split("\n")
+    lines = []
+
+    for text in texts:
+        text = text.rstrip(" ")
+
+        if not text:
+            lines.append("")
+            continue
+
+        # Preserve leading spaces in all cases.
+        a = len(text) - len(text.lstrip(" "))
+
+        # At any time, a is the rightmost known index you can legally split a line. I.e. it's legal
+        # to add text[:a] to lines, and line is what will be added to lines if
+        # text is split at a.
+        a = text.index(" ", a) if " " in text else len(text)
+        line = text[:a]
+
+        while a + 1 < len(text):
+            # b is the next legal place to break the line, with `bline`` the
+            # corresponding line to add.
+            if " " not in text[a + 1:]:
+                b = len(text)
+                bline = text
+                
+            else:
+                # Lines may be split at any space character that immediately follows a non-space
+                # character.
+                b = text.index(" ", a + 1)
+                while text[b - 1] == " ":
+                    if " " in text[b + 1:]:
+                        b = text.index(" ", b + 1)
+                    else:
+                        b = len(text)
+                        break
+                bline = text[:b]
+
+            bline = text[:b]
+
+            if font.size(bline)[0] <= max_width:
+                a, line = b, bline
+
+            else:
+                lines.append(line)
+                text = text[a:].lstrip(" ")
+                a = text.index(" ", 1) if " " in text[1:] else len(text)
+                line = text[:a]
+
+        if text:
+            lines.append(line)
+    return lines
+
+def create_text(text, font, color, pos, max_width=None):
+    if max_width is not None:
+        lines = wrap_text(text, font, max_width)
+    else:
+        lines = text.replace("\t", "    ").split("\n")
+
+    line_ys = (
+        np.arange(len(lines))
+    ) * 1.05 * font.get_linesize() + pos[1]
+
+    # Create the surface and rect that make up each line
+    text_objects = []
+
+    for line, y_pos in zip(lines, line_ys):
+        text_surface = font.render(line, True, color)
+        text_objects.append((text_surface, (pos[0], y_pos)))
+
+    return text_objects
 
 pygame.init()
 size = width, height = 480, 320
@@ -21,10 +95,24 @@ afternoonWeatherIcon = load_svg("svg/wi-day-cloudy.svg")
 eveningWeatherIcon = load_svg("svg/wi-night-cloudy.svg")
 nightWeatherIcon = load_svg("svg/wi-night-clear.svg")
 
+sunriseIcon = load_svg("svg/wi-sunrise.svg")
+sunsetIcon = load_svg("svg/wi-sunset.svg")
+moonriseIcon = load_svg("svg/wi-moonrise.svg")
+moonsetIcon = load_svg("svg/wi-moonset.svg")
+
+sunriseIcon = pygame.transform.scale(sunriseIcon, (30, 30))
+sunsetIcon = pygame.transform.scale(sunsetIcon, (30, 30))
+moonriseIcon = pygame.transform.scale(moonriseIcon, (30, 30))
+moonsetIcon = pygame.transform.scale(moonsetIcon, (30, 30))
+
+moonStateIcon = load_svg("svg/wi-moon-new.svg")
+
 morningWeatherIconRect = morningWeatherIcon.get_rect(center = (0 + 60, 136))
 afternoonWeatherIconRect = afternoonWeatherIcon.get_rect(center = (120 + 60, 136))
 eveningWeatherIconRect = eveningWeatherIcon.get_rect(center = (240 + 60, 136))
 nightWeatherIconRect = nightWeatherIcon.get_rect(center = (360 + 60, 136))
+
+moonStateIconRect = moonStateIcon.get_rect(center = (215 + 50, 236))
 
 font_18 = pygame.font.Font('jbm.ttf', 18)
 font_14 = pygame.font.Font('jbm.ttf', 14)
@@ -56,6 +144,29 @@ afternoonWind = font_8.render('SW @ 13.3 km/h', True, (255, 255, 255), (0, 0, 0)
 eveningWind = font_8.render('S @ 10.4 km/h', True, (255, 255, 255), (0, 0, 0))
 nightWind = font_8.render('SW @ 13.3 km/h', True, (255, 255, 255), (0, 0, 0))
 
+sunriseLabel = font_8.render('Sunrise', True, (255, 255, 255), (0, 0, 0))
+sunsetLabel = font_8.render('Sunset', True, (255, 255, 255), (0, 0, 0))
+moonriseLabel = font_8.render('Moonrise', True, (255, 255, 255), (0, 0, 0))
+moonsetLabel = font_8.render('Moonset', True, (255, 255, 255), (0, 0, 0))
+
+sunsetTime = font_10.render('06:57 PM', True, (255, 255, 255), (0, 0, 0))
+sunriseTime = font_10.render('07:07 AM', True, (255, 255, 255), (0, 0, 0))
+moonriseTime = font_10.render('09:28 PM', True, (255, 255, 255), (0, 0, 0))
+moonsetTime = font_10.render('09:57 AM', True, (255, 255, 255), (0, 0, 0))
+
+moonStateLabel = font_10.render('New Moon', True, (255, 255, 255), (0, 0, 0))
+
+techNewsLabel = font_8.render('Random Tech News', True, (255, 255, 255), (0, 0, 0))
+techNewsTitle = create_text(
+    text="Amazon Union Loses Vote at Second Staten Island Warehouse",
+    font=font_10,
+    color=(255, 255, 255),  # White
+    pos=(325, 230),  # Center of the screen
+    max_width=140,
+)
+
+currencyConvertion = font_10.render('1 USD = 4.35 MYR', True, (255, 255, 255), (0, 0, 0))
+
 currentWeatherDescriptionRect = currentWeatherDescription.get_rect(center = (80, 64))
 currentTimeRect = currentTime.get_rect(center = (320+80, 32))
 currentZoneRect = currentZone.get_rect(center = (320+80, 48))
@@ -74,6 +185,10 @@ morningWindRect = morningWind.get_rect(center = (0+60, 186))
 afternoonWindRect = afternoonWind.get_rect(center = (120+60, 186))
 eveningWindRect = eveningWind.get_rect(center = (240+60, 186))
 nightWindRect = nightWind.get_rect(center = (360+60, 186))
+
+moonStateLabelRect = moonStateLabel.get_rect(center = (215+50, 274))
+
+currencyConvertionRect = currencyConvertion.get_rect(center = (0+67.5, 288+16))
 
 while 1:
     screen.fill((0, 0, 0))
@@ -111,9 +226,37 @@ while 1:
     screen.blit(eveningWind, eveningWindRect)
     screen.blit(nightWind, nightWindRect)
 
+    screen.blit(sunriseIcon, (20, 216))
+    screen.blit(sunsetIcon, (20, 246))
+    screen.blit(moonriseIcon, (110, 216))
+    screen.blit(moonsetIcon, (110, 246))
+
+    screen.blit(sunriseLabel, (54, 218))
+    screen.blit(sunsetLabel, (54, 248))
+    screen.blit(moonriseLabel, (140, 218))
+    screen.blit(moonsetLabel, (140, 248))
+
+    screen.blit(sunriseTime, (54, 230))
+    screen.blit(sunsetTime, (54, 260))
+    screen.blit(moonriseTime, (140, 230))
+    screen.blit(moonsetTime, (140, 260))
+
+    screen.blit(moonStateIcon, moonStateIconRect)
+    screen.blit(moonStateLabel, moonStateLabelRect)
+
+    screen.blit(techNewsLabel, (325, 214))
+    for text_object in techNewsTitle[:3]:
+        screen.blit(*text_object)
+
+    screen.blit(currencyConvertion, currencyConvertionRect)
+
     pygame.draw.line(screen, (255, 255, 255), (0, 80), (480, 80), 1)
     pygame.draw.line(screen, (255, 255, 255), (160, 0), (160, 80), 1)
     pygame.draw.line(screen, (255, 255, 255), (320, 0), (320, 80), 1)
     pygame.draw.line(screen, (255, 255, 255), (0, 202), (480, 202), 1)
+    pygame.draw.line(screen, (255, 255, 255), (0, 288), (480, 288), 1)
+    pygame.draw.line(screen, (255, 255, 255), (215, 202), (215, 288), 1)
+    pygame.draw.line(screen, (255, 255, 255), (315, 202), (315, 288), 1)
+    pygame.draw.line(screen, (255, 255, 255), (135, 288), (135, 320), 1)
 
     pygame.display.flip()
