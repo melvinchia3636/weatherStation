c902f99248c290144f9f39be373caa46e8188833
melvinchia3636 <melvinchia623600@gmail.com>
2022-05-05 10:26:06 +0800

    fullscreen

--- main.py
+++ main.py
@@ -93,8 +93,8 @@ class WeatherStation:
         pygame.init()
 
         size = 480, 320
-        # self.screen = self.display.set_mode(size, self.FULLSCREEN)
-        self.screen = pygame.display.set_mode(size)
+        self.screen = self.display.set_mode(size, self.FULLSCREEN)
+        # self.screen = pygame.display.set_mode(size)
         pygame.display.set_caption("Weather Station")
 
         pygame.mouse.set_visible(False)
